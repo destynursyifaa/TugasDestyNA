83abeb42c0a1b0b0918ece14d5d303e04fac1db4
madesuntyadevi <32846022+madesuntyadevi@users.noreply.github.com>
2017-11-07 21:51:18 -0800

    input nilai

--- No1.cpp
+++ No1.cpp
@@ -5,5 +5,21 @@
 using namespace std;
 
 int main () {
+	int x, y, z;
+	cout << "Input nilai x = ";
+	cin  >> x;
+	
+	cout << "Input nilai y = ";
+	cin  >> y;
+	
+		if (x > y)	{
+	z = y - x;	
+		cout<<abs (z)<<endl;
+		cout<<"Hasil Positif"; {
+	
 }
+	
+	
+
+	
 
